f60797677059485e7a9572dd2c9eb0fd1c12f7e4
Med Mahmoud <medthehatta@gmail.com>
2013-05-08 19:21:14 -0400

    Remove game dependence of pkl funcs

--- pkldump.py
+++ pkldump.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python -i
 
 import dfrpgmon2 as dm
+import pickle
 import yaml
 import sys
 
@@ -8,9 +9,7 @@ if len(sys.argv)<2:
   print("Not enough arguments")
   sys.exit(1)
 
-pickle = sys.argv[1]
+pfile = sys.argv[1]
 
-def j(c): dm.run_command(c,"med","##dfrpg",dm.GAME,dm.COMMANDS)
-j(".load {0}".format(pickle))
-yaml.dump(dm.GAME,open("{0}-dump.yml".format(pickle),'w'),yaml.CDumper)
+yaml.dump(pickle.load(open(pfile,'rb')),open("{0}-dump.yml".format(pfile),'w'),yaml.CDumper)
 
--- pklload.py
+++ pklload.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python -i
 
 import dfrpgmon2 as dm
+import pickle
 import yaml
 import sys
 
@@ -8,12 +9,9 @@ if len(sys.argv)<2:
   print("Not enough arguments")
   sys.exit(1)
 
-pickle = sys.argv[1]
+pfile = sys.argv[1]
 
-def j(c): dm.run_command(c,"med","##dfrpg",dm.GAME,dm.COMMANDS)
-j(".load {0}".format(pickle))
-GAME = yaml.load(open("{0}-dump.yml".format(pickle)),yaml.CLoader)
-GAME.save()
+pickle.dump(yaml.load(open("{0}-dump.yml".format(pfile)),yaml.CLoader),open(pfile,'wb'))
 
 
 
