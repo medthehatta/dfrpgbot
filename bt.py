33a0a505fa79970698e9efd77a3e2302dd4f8011
Med Mahmoud <medthehatta@gmail.com>
2013-05-08 20:08:40 -0400

    More options in bt

--- bt.py
+++ bt.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python -i
 
 import dfrpgmon2 as dm
-def j(c): dm.run_command(c,"med",dm.GAME,dm.COMMANDS)
+def j(c): dm.run_command(c,"med","##dfrpg",dm.GAME,dm.COMMANDS)
+def k(c): dm.run_command(c,"med","##dfrpg2",dm.GAME,dm.COMMANDS)
+def h(c): dm.run_command(c,"bob","##dfrpg",dm.GAME,dm.COMMANDS)
 
